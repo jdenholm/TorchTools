ae81f810b702844633ef72bbecc0a187af83828b
jdenholm <j.denholm.2017@gmail.com>
2022-10-17 21:33:53 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Setup for `TorchTools`."""
+from setuptools import setup
+
+setup(
+    name="torch_tools",
+    package_dir={"torch_tools": "torch_tools"},
+    author="Jim Denholm",
+    version="0.0.0",
+)
