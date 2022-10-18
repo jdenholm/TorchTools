639fa31cacdb8698be6faf77e9c0edfae1d317c3
jdenholm <j.denholm.2017@gmail.com>
2022-10-18 20:42:04 +0100

    Added tests for argument processing

---
+++ tests/models/test_argument_processing.py
@@ -0,0 +1,46 @@
+"""Tests for functions in `torch_tools.models._argument_processing`."""
+import pytest
+
+
+from torch_tools.models import _argument_processing as ap
+
+
+def test_process_num_feats_types():
+    """Test `ap.process_num_feats`'s type checking."""
+    # Should work with int
+    _ = ap.process_num_feats(1)
+
+    # Should break with any non-int
+    with pytest.raises(TypeError):
+        _ = ap.process_num_feats(1.0)
+    with pytest.raises(TypeError):
+        _ = ap.process_num_feats("1.0")
+    with pytest.raises(TypeError):
+        _ = ap.process_num_feats(1.0j)
+
+
+def test_process_num_feats_values():
+    """test `ap.process_num_feats`'s value checking."""
+    # Should work with ints of one and above
+    _ = ap.process_num_feats(1)
+    _ = ap.process_num_feats(2)
+
+    # Should fail with anything less than one
+    with pytest.raises(ValueError):
+        _ = ap.process_num_feats(0)
+    with pytest.raises(ValueError):
+        _ = ap.process_num_feats(-1)
+
+def test_process_boolean_arg_types():
+    """Test `ap.process_boolean_arg`'s type checking."""
+    # Should work with bool
+    _ = ap.process_boolean_arg(True)
+    _ = ap.process_boolean_arg(False)
+
+    # Should fail with non-bool
+    with pytest.raises(TypeError):
+        _ = ap.process_boolean_arg(1)
+    with pytest.raises(TypeError):
+        _ = ap.process_boolean_arg(1.0)
+    with pytest.raises(TypeError):
+        _ = ap.process_boolean_arg("Batman")
