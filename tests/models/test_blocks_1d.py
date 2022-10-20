b0565c5af798578c309fd44bc3c973a07dd39f79
jdenholm <j.denholm.2017@gmail.com>
2022-10-20 20:53:51 +0100

    Added more tests for DenseBlock arguments

--- tests/models/test_blocks_1d.py
+++ tests/models/test_blocks_1d.py
@@ -58,7 +58,7 @@ def test_dense_block_out_feats_values():
         _ = DenseBlock(in_feats=10, out_feats=-1)
 
 
-def test_dense_blocks_batch_norm_arg_types():
+def test_dense_block_batch_norm_arg_types():
     """Test types accepted by `batch_norm` arg of `DenseBlock`."""
     # Should work with bool
     _ = DenseBlock(in_feats=10, out_feats=1, batch_norm=True)
@@ -71,3 +71,43 @@ def test_dense_blocks_batch_norm_arg_types():
         _ = DenseBlock(in_feats=10, out_feats=1, batch_norm="True")
     with pytest.raises(TypeError):
         _ = DenseBlock(in_feats=10, out_feats=1, batch_norm=10.0)
+
+
+def test_dense_block_dropout_prob_arg_type():
+    """Type-check `dropout_prob` argument of `DenseBlock`."""
+    # Should work with float on [0..0, 1.0)
+    _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=0.0)
+    _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=0.999)
+
+    # Should break with non-float
+    with pytest.raises(TypeError):
+        _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=0)
+    with pytest.raises(TypeError):
+        _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob="1")
+
+
+def test_dense_block_dropout_prob_values():
+    """Value-check `dropouut_prob` argument of `DenseBlock`."""
+    # Should work with float on [0.0, 1.0)
+    _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=0.0)
+    _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=0.999)
+
+    # Should break with floats not on [0.0, 1.0)
+    with pytest.raises(ValueError):
+        _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=-0.00001)
+    with pytest.raises(ValueError):
+        _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=1.0)
+    with pytest.raises(ValueError):
+        _ = DenseBlock(in_feats=10, out_feats=2, dropout_prob=1.00001)
+
+
+def test_dense_block_final_block_argument_type():
+    """Type-check the `final_block` argument of `DenseBlock`."""
+    # Should work with bool
+    _ = DenseBlock(in_feats=10, out_feats=2, final_block=True)
+    _ = DenseBlock(in_feats=10, out_feats=2, final_block=False)
+
+    with pytest.raises(TypeError):
+        _ = DenseBlock(in_feats=10, out_feats=2, final_block=1)
+    with pytest.raises(TypeError):
+        _ = DenseBlock(in_feats=10, out_feats=2, final_block="True")
