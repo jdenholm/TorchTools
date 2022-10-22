c440a17580f5d7e9cc40fe90bbc34646931bb625
jdenholm <j.denholm.2017@gmail.com>
2022-10-22 20:51:16 +0100

    Added tests for the arguments of InputBlock

--- tests/models/test_blocks_1d_args.py
+++ tests/models/test_blocks_1d_args.py
@@ -1,7 +1,7 @@
 """Test for blocks in `torch_tools.models.._blocks_1d`."""
 import pytest
 
-from torch_tools.models._blocks_1d import DenseBlock
+from torch_tools.models._blocks_1d import DenseBlock, InputBlock
 
 
 def test_dense_block_in_feats_types():
@@ -124,3 +124,75 @@ def test_dense_block_negative_slope_argument_type():
         _ = DenseBlock(in_feats=10, out_feats=2, negative_slope=1)
     with pytest.raises(TypeError):
         _ = DenseBlock(in_feats=10, out_feats=2, negative_slope=1j)
+
+
+def test_input_block_in_feats_arg_types():
+    """Type-check the `in_feats` argument of `InputBlock`."""
+    # Should work with int
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.1)
+
+    # Should break with any non-int
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10.0, batch_norm=True, dropout=0.1)
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10j, batch_norm=True, dropout=0.1)
+
+
+def test_input_block_in_feats_arg_values():
+    """Value-check the `in_feats` argument of `InputBlock`."""
+    # Should work with int of one or more
+    _ = InputBlock(in_feats=1, batch_norm=True, dropout=0.1)
+    _ = InputBlock(in_feats=2, batch_norm=True, dropout=0.1)
+
+    # Should break with ints less than one
+    with pytest.raises(ValueError):
+        _ = InputBlock(in_feats=0, batch_norm=True, dropout=0.1)
+    with pytest.raises(ValueError):
+        _ = InputBlock(in_feats=-1, batch_norm=True, dropout=0.1)
+
+
+def test_input_block_batch_norm_arg_types():
+    """Type-check the `batch_norm` argument of `InputBlock`."""
+    # Should only work with boool
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.5)
+    _ = InputBlock(in_feats=10, batch_norm=False, dropout=0.5)
+
+    # Should break with non-bool
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10, batch_norm=1, dropout=0.5)
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10, batch_norm=1j, dropout=0.5)
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10, batch_norm="Gandalf", dropout=0.5)
+
+
+def test_input_block_dropout_arg_types():
+    """Type-check the `dropout` argument of `InputBlock`."""
+    # Should work with floats on [0.0, 1.0)
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.0)
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.5)
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.999)
+
+    # Should break with non-float
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10, batch_norm=True, dropout=0)
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10, batch_norm=True, dropout="Saruman")
+    with pytest.raises(TypeError):
+        _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.5j)
+
+
+def test_input_block_arg_values():
+    """Value-check the `dropout` argument of `InputBlock`."""
+    # Should work with floats on [0.0, 1.0)
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.0)
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.5)
+    _ = InputBlock(in_feats=10, batch_norm=True, dropout=0.999)
+
+    # Should break with floats not on [0.0, 1.0)
+    with pytest.raises(ValueError):
+        _ = InputBlock(in_feats=10, batch_norm=True, dropout=-0.00001)
+    with pytest.raises(ValueError):
+        _ = InputBlock(in_feats=10, batch_norm=True, dropout=1.0)
+    with pytest.raises(ValueError):
+        _ = InputBlock(in_feats=10, batch_norm=True, dropout=1.00001)
