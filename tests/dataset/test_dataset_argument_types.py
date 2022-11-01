40ea23d7401ca12743d238d7b83534308a5faebd
jdenholm <j.denholm.2017@gmail.com>
2022-11-01 19:26:13 +0000

    Added more argument tests

--- tests/dataset/test_dataset_argument_types.py
+++ tests/dataset/test_dataset_argument_types.py
@@ -5,13 +5,14 @@ from string import ascii_lowercase
 import numpy as np
 import torch
 
+import pytest
+
 from torch_tools.datasets import DataSet
 
 
-def test_allowed_input_types():
-    """Test `DataSet` works with the allowed input types."""
+def test_inputs_with_allowed_types():
+    """Test `DataSet` accepts `inputs` with allowed types."""
     strings = list(map(lambda x: str(x) + ".png", ascii_lowercase))
-    print(strings)
     # Should work with `typing.Sequence` of:
     # `str`
     _ = DataSet(inputs=strings)
@@ -21,3 +22,26 @@ def test_allowed_input_types():
     _ = DataSet(inputs=list(np.zeros((10, 2))))
     # `torch.Tensor`
     _ = DataSet(inputs=list(torch.zeros(10, 2)))
+
+
+def test_inputs_with_forbidden_types():
+    """Test `DataSet` catches `inputs` with forbidden types."""
+    # Should break with non-sequence
+    with pytest.raises(TypeError):
+        _ = DataSet(inputs=1)
+    with pytest.raises(TypeError):
+        _ = DataSet({"Not sequence": 1})
+
+    # Should break with Sequence containing which doesn't contain
+    # 'str', 'Path', 'numpy.ndarray', 'torch.Tensor'
+    with pytest.raises(TypeError):
+        _ = DataSet(inputs=[1, 2, 3, 4, 5, 6])
+    with pytest.raises(TypeError):
+        _ = DataSet(inputs=(((1,)), (2,), (3,)))
+
+
+def test_inputs_with_inconsistent_allowed_types():
+    """Test `DataSet` catches when `inputs` has inconsisten types."""
+    # Should break with allowed types if they are inconsistent
+    with pytest.raises(TypeError):
+        _ = DataSet(inputs=["mixed", Path("inputs")])
