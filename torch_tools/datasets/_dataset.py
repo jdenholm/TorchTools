eaf5da59f163c4c1739998a551981c42ad907542
jdenholm <j.denholm.2017@gmail.com>
2022-11-18 22:10:51 +0000

    Added transforms for both inputs and targets

--- torch_tools/datasets/_dataset.py
+++ torch_tools/datasets/_dataset.py
@@ -3,7 +3,7 @@ from typing import Sequence, Union, Optional, Tuple
 from pathlib import Path
 
 
-from torch import Tensor
+from torch import Tensor, concat
 from torchvision.transforms import Compose
 
 
@@ -35,7 +35,7 @@ class DataSet(_BaseDataset):
         A composition of transforms to apply to both the input and target.
         Note: these transfroms are applied after `input_tfms` and
         `target_tfms`, at which point the inputs and targets should be tensors.
-        Each input--target pair will be concatenated along `dim=1`,
+        Each input--target pair will be concatenated along `dim=0`,
         transformed, and sliced apart, in the way one would apply rotations or
         reflections to images and segmentation masks. The dimensionality
         matters!
@@ -65,6 +65,27 @@ class DataSet(_BaseDataset):
         self._x_tfms = self._receive_tfms(input_tfms)
         self._y_tfms = self._receive_tfms(target_tfms)
         self._both_tfms = self._receive_tfms(both_tfms)
+
+    @staticmethod
+    def _receive_tfms(tfms: Optional[Compose] = None) -> Union[Compose, None]:
+        """Check the transforms are `Compose` (or `None`) and return them.
+
+        Parameters
+        ----------
+        tfms : Optional[Compose]
+            The transfroms to check and return.
+
+        Raises
+        ------
+        TypeError
+            If `tfms` is not a `Compose`.
+
+        """
+        if not isinstance(tfms, (Compose, type(None))):
+            msg = "Transforms should be wrapped in a 'Compose', or 'None'. "
+            msg += f"Got '{type(tfms)}'."
+            raise TypeError(msg)
+        return tfms
 
     def _apply_input_tfms(
         self,
@@ -103,26 +124,38 @@ class DataSet(_BaseDataset):
         """
         return self._y_tfms(y_item) if self._y_tfms is not None else y_item
 
-    @staticmethod
-    def _receive_tfms(tfms: Optional[Compose] = None) -> Union[Compose, None]:
-        """Check the transforms are `Compose` (or `None`) and return them.
+    def _apply_both_tfms(
+        self,
+        x_item: Tensor,
+        y_item: Tensor,
+    ) -> Tuple[Tensor, Tensor]:
+        """Apply `self._both_tfms` to `x_item` and `y_item`.
 
         Parameters
         ----------
-        tfms : Optional[Compose]
-            The transfroms to check and return.
+        x_item : Tensor
+            Input item.
+        y_item : Tensor
+            Target item.
 
-        Raises
-        ------
-        TypeError
-            If `tfms` is not a `Compose`.
+        Returns
+        -------
+        Tensor
+            Input item.
+        Tensor
+            Target item.
+
+        Notes
+        -----
+        `x_item` and `y_item` are concatenated along the channel dimension,
+        transformed and then sliced apart.
 
         """
-        if not isinstance(tfms, (Compose, type(None))):
-            msg = "Transforms should be wrapped in a 'Compose', or 'None'. "
-            msg += f"Got '{type(tfms)}'."
-            raise TypeError(msg)
-        return tfms
+        if self._both_tfms is not None:
+            slice_idx = x_item.shape[0]
+            transformed = self._both_tfms(concat([x_item, y_item], dim=0))
+            return transformed[:slice_idx], transformed[slice_idx:]
+        return x_item, y_item
 
     def __getitem__(self, idx: int) -> Union[Tuple[Tensor, ...], Tensor]:
         """Return an input-target pair (or just an input).
@@ -139,4 +172,6 @@ class DataSet(_BaseDataset):
             return x_item
 
         y_item = self._apply_target_transforms(self.targets[idx])
+        x_item, y_item = self._apply_both_tfms(x_item, y_item)
+
         return x_item, y_item
