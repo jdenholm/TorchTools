332f6d26e9fa956dc0d57051c1f3cb1d201b2dc6
jdenholm <j.denholm.2017@gmail.com>
2022-12-04 21:35:39 +0000

    Renamed conv_net_2d module

--- torch_tools/models/__init__.py
+++ torch_tools/models/__init__.py
@@ -1,3 +1,3 @@
 """Init for `torch_tools.models`."""
 from torch_tools.models._dense_network import DenseNetwork
-from torch_tools.models._cnn_network import ConvNet2d
+from torch_tools.models._conv_net_2d import ConvNet2d
