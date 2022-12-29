39dd1c6f152093c755e38df317875f091b773edb
jdenholm <j.denholm.2017@hotmail.com>
2022-12-29 11:40:54 +0000

    Docstring typo

--- torch_tools/models/_blocks_2d.py
+++ torch_tools/models/_blocks_2d.py
@@ -235,7 +235,7 @@ class UNetUpBlock(Module):
 
     @staticmethod
     def _process_in_chans(in_chans: int) -> int:
-        """ "Process `in_chans` arg.
+        """Process `in_chans` arg.
 
         Parameters
         ----------
