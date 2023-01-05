bc25b9b2877838e23a9213aa9cf10bffb0776d9f
jdenholm <j.denholm.2017@gmail.com>
2023-01-05 19:30:02 +0000

    Started adding basic parts of the UNet

--- torch_tools/models/_unet.py
+++ torch_tools/models/_unet.py
@@ -2,15 +2,68 @@
 
 from torch import Tensor
 
-from torch.nn import Module
+from torch.nn import Module, Conv2d
+
+from torch_tools.models._argument_processing import (
+    process_num_feats,
+    process_negative_slope_arg,
+)
+
+from torch_tools.models._blocks_2d import DoubleConvBlock
+
+
+# pylint : disable=too-many-arguments
 
 
 class UNet(Module):
-    """UNet model for semantic segmentation."""
+    """UNet model for semantic segmentation.
 
-    def __init__(self):
+    Parameters
+    ----------
+    in_chans : int
+        The number of input channels.
+    out_chans : int
+        The number of output channels.
+    features_start : int
+        The number of features produced by the first convolutional block.
+    num_layers : int
+        The number of layers in the `UNet`.
+    pool_style : str
+        The pool style to use in the `DownBlock`s. Can be `"max"` or `"avg"`.
+    bilinear : bool
+        Whether to use use bilinear interpolation in the upsampling layers or
+        not. If `True`, we use bilinear interpolation to upsample. If `False`,
+        we use `ConvTranspose2d`.
+    lr_slope : float
+        The negative slope argument for `LeakyReLU` layers.
+
+    """
+
+    def __init__(
+        self,
+        in_chans: int,
+        out_chans: int,
+        features_start: int = 64,
+        num_layers: int = 4,
+        pool_style: str = "max",
+        bilinear: bool = False,
+        lr_slope: float = 0.1,
+    ):
         """Build `UNet`."""
         super().__init__()
+
+        self._in_conv = DoubleConvBlock(
+            in_chans=process_num_feats(in_chans),
+            out_chans=process_num_feats(features_start),
+            lr_slope=process_negative_slope_arg(lr_slope),
+        )
+
+        self._out_conv = Conv2d(
+            process_num_feats(features_start),
+            process_num_feats(out_chans),
+            kernel_size=1,
+            stride=1,
+        )
 
     def forward(self, batch: Tensor) -> Tensor:
         """Pass `batch` through the model.
