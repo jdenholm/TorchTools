ef063e9f47c2aad09ecb06c1d289ef74f076951e
jdenholm <j.denholm.2017@gmail.com>
2023-01-06 17:18:10 +0000

    Added more call tests for blocks 2d

--- tests/models/test_blocks_2d_call.py
+++ tests/models/test_blocks_2d_call.py
@@ -3,6 +3,7 @@
 from torch import rand  # pylint: disable=no-name-in-module
 
 from torch_tools.models._blocks_2d import ConvBlock, DoubleConvBlock, ResBlock
+from torch_tools.models._blocks_2d import DownBlock, UpBlock
 
 
 def test_conv_block_call_return_shapes_with_batchnorm_and_leaky_relu():
@@ -76,3 +77,25 @@ def test_res_block_call_return_shapes():
 
     block = ResBlock(in_chans=111)
     assert block(rand(10, 111, 50, 100)).shape == (10, 111, 50, 100)
+
+
+def test_down_block_call_return_shapes():
+    """Test the return shapes of `DownBlock`."""
+    block = DownBlock(in_chans=3, out_chans=8, pool="max", lr_slope=0.1)
+    assert block(rand(10, 3, 50, 100)).shape == (10, 8, 25, 50)
+
+    block = DownBlock(in_chans=3, out_chans=3, pool="avg", lr_slope=0.1)
+    assert block(rand(10, 3, 100, 50)).shape == (10, 3, 50, 25)
+
+    # Test with odd image sizes
+    block = DownBlock(in_chans=3, out_chans=3, pool="max", lr_slope=0.1)
+    assert block(rand(10, 3, 101, 51)).shape == (10, 3, 50, 25)
+
+
+def test_up_block_call_return_shapes():
+    """Test the return shapes produced by `UpBlock`."""
+    block = UpBlock(in_chans=3, out_chans=8, bilinear=True)
+    assert block(rand(10, 3, 16, 32)).shape == (10, 8, 32, 64)
+
+    block = UpBlock(in_chans=3, out_chans=3, bilinear=False)
+    assert block(rand(10, 3, 128, 256)).shape == (10, 3, 256, 512)
