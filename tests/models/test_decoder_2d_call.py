05dc526c65b741d656bfca36219d829b1b8fae48
jdenholm <j.denholm.2017@gmail.com>
2023-01-25 11:21:50 +0000

    Adding tests for decoder 2d

--- tests/models/test_decoder_2d_call.py
+++ tests/models/test_decoder_2d_call.py
@@ -20,3 +20,28 @@ def test_decoder_2d_returns_images_of_the_right_shape():
 
     mini_batch = rand(10, 512, 4, 8)
     assert decoder(mini_batch).shape == (10, 111, 64, 128)
+
+
+def test_decoder_2d_output_sizes_at_each_block():
+    """Test the output sizes produced by each ``UpBlock``."""
+    decoder = Decoder2d(
+        in_chans=512,
+        out_chans=111,
+        num_blocks=5,
+        bilinear=False,
+        lr_slope=0.123456,
+    )
+
+    chans = 512
+    height, width = 8, 8
+    batch = rand(10, chans, height, width)
+
+    for block in list(decoder.children())[:-1]:
+
+        batch = block(batch)
+
+        chans //= 2
+        height *= 2
+        width *= 2
+
+        assert batch.shape == (10, chans, height, width)
