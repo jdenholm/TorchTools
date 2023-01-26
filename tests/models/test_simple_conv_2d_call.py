93c4e2ace19f17daf35df7e3bb709b58f1ab5311
jdenholm <j.denholm.2017@gmail.com>
2023-01-26 23:31:58 +0000

    Adding tests for simple conv net 2d call

--- tests/models/test_simple_conv_2d_call.py
+++ tests/models/test_simple_conv_2d_call.py
@@ -1,1 +1,16 @@
-"""Test the call behaviour of ``torch_tools.models.SimpleConvNet2d``."""
+"""Test the call behaviour of ``torch_tools.SimpleConvNet2d``."""
+from torch import rand  # pylint: disable=no-name-in-module
+
+from torch_tools import SimpleConvNet2d
+
+
+def test_accepted_input_channels():
+    """Test the number of inputs channels accepted."""
+    model = SimpleConvNet2d(in_chans=123, out_feats=8)
+    _ = model(rand(10, 123, 50, 50))
+
+    model = SimpleConvNet2d(in_chans=321, out_feats=8)
+    _ = model(rand(10, 321, 50, 50))
+
+    model = SimpleConvNet2d(in_chans=666, out_feats=8)
+    _ = model(rand(10, 666, 50, 50))
