70e3127c9e2b5665b2d3a00050e0d5ee80f6f80c
jdenholm <j.denholm.2017@gmail.com>
2023-04-10 14:24:23 +0100

    Experimenting with auto docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/bash
 
+
+git config user.name "jdenholm"
+git config user.email "j.denholm.2017@gmail.com"
+
+current_branch="$(git branch --show-current)"
+target_branch="doc-branch"
+
+
+# Build the html
 cd docs
 make clean
 make html
 
-target_branch="doc-branch"
 
-
-# Create the documentation branch
+# Create the documentation branch and update it with current_branch
 git checkout $target_branch
-shopt -s extglob
-git rm -rf !("docs")
+git merge $doc_branch
 
+# Delete everything except the docs folder
+rm -r !("docs")
 
 cd docs
 cp _build/html/*.html .
@@ -19,8 +27,5 @@ cp _build/html/*.html .
 cd ..
 git add --all
 
-git config user.name "jdenholm"
-git config user.email "j.denholm.2017@gmail.com"
-
 git commit -m "Updated docs"
-git push -u origin $target_branch
+git push -u origin $target_branch --force
