a58f30bd71c656bd464401aa7f14802ead04bfb3
jdenholm <j.denholm.2017@gmail.com>
2023-04-10 16:01:35 +0100

    Updated docs

--- build_docs.sh
+++ build_docs.sh
@@ -15,7 +15,7 @@ make html
 
 
 # Create the documentation branch and update it with current_branch
-git checkout --orphan $target_branch
+git checkout $target_branch
 
 shopt -s extglob
 git rm -rf !("docs")
