a882cacd988f0aa1ca0ad1873f60660840438cb4
jdenholm <j.denholm.2017@gmail.com>
2023-04-16 15:05:11 +0100

    :memo: Modified doc building

--- build_docs.sh
+++ build_docs.sh
@@ -7,15 +7,6 @@ git config user.email "j.denholm.2017@gmail.com"
 current_branch="$(git branch --show-current)"
 target_branch="doc-branch"
 
-
-# Build the html
-cd docs
-make clean
-make html
-cp -r _build/html/*.html .
-cd ..
-
-
 git pull --all
 
 git branch -c $current_branch $target_branch
