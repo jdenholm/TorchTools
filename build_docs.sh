7629c96d58d24b451b7e8f9318eb77a34a4624f1
jdenholm <j.denholm.2017@gmail.com>
2023-04-24 20:04:40 +0100

    :construction worker: :alembic: Experimenting with docs deploy

--- build_docs.sh
+++ build_docs.sh
@@ -7,25 +7,15 @@ git config user.email "j.denholm.2017@gmail.com"
 current_branch="$(git branch --show-current)"
 target_branch="doc-branch"
 
-# echo $current_branch
-echo "branch=$(echo ${GITHUB_REF#refs/heads/})"
+# Build the html
+cd docs
+make clean
+make html
+cp -r _build/html/*.html .
+cd ..
 
 
-# git checkout $current_branch
-# git checkout -b $target_branch
+git add --all
 
-# # Build the html
-# cd docs
-# make clean
-# make html
-# cp -r _build/html/*.html .
-# cd ..
-
-
-# shopt -s extglob
-# rm -rf !("docs")
-
-# git add --all
-
-# git commit -m "Updated docs"
-# git push -u origin $target_branch --force
+git commit -m "Updated docs"
+git push -u origin $target_branch --force
