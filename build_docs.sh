f9cc3e033810e15fbf3a67f9b4e8fae2228c73b9
jdenholm <j.denholm.2017@gmail.com>
2023-04-24 20:13:02 +0100

    :construction worker: :alembic: Experimenting with docs deploy

--- build_docs.sh
+++ build_docs.sh
@@ -4,9 +4,6 @@
 git config user.name "jdenholm"
 git config user.email "j.denholm.2017@gmail.com"
 
-current_branch="$(git branch --show-current)"
-target_branch="doc-branch"
-
 # Build the html
 cd docs
 make clean
@@ -18,4 +15,4 @@ cd ..
 git add --all
 
 git commit -m "Updated docs"
-git push -u origin $target_branch --force
+git push
