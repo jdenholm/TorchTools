527cc32b17f9505a2fe0e0f1f9744fcb7d440dea
jdenholm <j.denholm.2017@gmail.com>
2023-04-24 21:13:01 +0100

    :construction worker: :alembic: Experimenting with docs deploy

---
+++ build_docs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+
+git config user.name "jdenholm"
+git config user.email "j.denholm.2017@gmail.com"
+
+source_branch=$GITHUB_HEAD_REF
+target_branch=$GITHUB_BASE_REF
+
+git checkout origin $source_branch
+
+# Build the html
+cd docs
+make clean
+make html
+cp -r _build/html/*.html .
+cd ..
+
+
+# Note: if you are merging from dev to main, source_branch would be dev and
+# target branch would be main.
+
+
+
+git add --all
+
+git commit -m "Updated docs"
+git push -u $source_branch
