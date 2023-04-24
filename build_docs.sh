432523e7935b179cee401e8e4f9303222b5bb7e3
jdenholm <j.denholm.2017@gmail.com>
2023-04-24 07:28:54 +0100

    :construction_worker: Working on doc deployment

--- build_docs.sh
+++ build_docs.sh
@@ -7,29 +7,26 @@ git config user.email "j.denholm.2017@gmail.com"
 current_branch="$(git branch --show-current)"
 target_branch="doc-branch"
 
+git pull --all
 
-git status
+git branch -c $current_branch $target_branch
+git checkout $target_branch
 
-# git pull --all
-
-# git branch -c $current_branch $target_branch
-# git checkout $target_branch
-
-# # Build the html
-# cd docs
-# make clean
-# make html
-# cp -r _build/html/*.html .
-# cd ..
+# Build the html
+cd docs
+make clean
+make html
+cp -r _build/html/*.html .
+cd ..
 
 
-# shopt -s extglob
-# rm -rf !("docs")
+shopt -s extglob
+rm -rf !("docs")
 
-# git add --all
+git add --all
 
-# git commit -m "Updated docs"
-# git push -u origin $target_branch --force
+git commit -m "Updated docs"
+git push -u origin $target_branch --force
 
-# git checkout $current_branch
-# git branch -D $target_branch
+git checkout $current_branch
+git branch -D $target_branch
