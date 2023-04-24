00da8b355d9832e0be3c348dc9fde82e91dca8ec
jdenholm <j.denholm.2017@gmail.com>
2023-04-24 16:13:14 +0100

    :construction_worker: Print out current branch

--- build_docs.sh
+++ build_docs.sh
@@ -7,26 +7,24 @@ git config user.email "j.denholm.2017@gmail.com"
 current_branch="$(git branch --show-current)"
 target_branch="doc-branch"
 
-git pull --all
-
-git branch -c $current_branch $target_branch
-git checkout $target_branch
-
-# Build the html
-cd docs
-make clean
-make html
-cp -r _build/html/*.html .
-cd ..
+echo $current_branch
 
 
-shopt -s extglob
-rm -rf !("docs")
+# git checkout $current_branch
+# git checkout -b $target_branch
 
-git add --all
+# # Build the html
+# cd docs
+# make clean
+# make html
+# cp -r _build/html/*.html .
+# cd ..
 
-git commit -m "Updated docs"
-git push -u origin $target_branch --force
 
-git checkout $current_branch
-git branch -D $target_branch
+# shopt -s extglob
+# rm -rf !("docs")
+
+# git add --all
+
+# git commit -m "Updated docs"
+# git push -u origin $target_branch --force
