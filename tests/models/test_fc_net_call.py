d63fe4025b5d29d7e08d2e65ee0c6adf0891fb3b
jdenholm <j.denholm.2017@gmail.com>
2023-04-25 11:13:37 +0100

    :truck: :construction: Renaming DenseNetwork to FCNet

--- tests/models/test_fc_net_call.py
+++ tests/models/test_fc_net_call.py
@@ -1,20 +1,20 @@
-"""Test calling `DenseNetwork` with different input and output features."""
+"""Test calling `FCNet` with different input and output features."""
 
 import pytest
 
 from torch import rand  # pylint: disable=no-name-in-module
 
-from torch_tools import DenseNetwork
+from torch_tools import FCNet
 
 
 def test_model_with_correct_input_features():
     """Test the model works with different input features."""
     # With input batchnorm and dropout
-    model = DenseNetwork(10, 2, hidden_sizes=(5, 5, 5))
+    model = FCNet(10, 2, hidden_sizes=(5, 5, 5))
     _ = model(rand(2, 10))
 
     # Without input batchnorm and dropout
-    model = DenseNetwork(
+    model = FCNet(
         20,
         2,
         hidden_sizes=(5, 5, 5),
@@ -27,12 +27,12 @@ def test_model_with_correct_input_features():
 def test_model_with_incorrect_input_features():
     """Test the model breaks when using the wrong number of input features."""
     # With input batchnorm and dropout
-    model = DenseNetwork(10, 2, hidden_sizes=(5, 5, 5))
+    model = FCNet(10, 2, hidden_sizes=(5, 5, 5))
     with pytest.raises(RuntimeError):
         _ = model(rand(2, 20))
 
     # Without input batchnorm and dropout
-    model = DenseNetwork(
+    model = FCNet(
         10,
         2,
         hidden_sizes=(5, 5, 5),
@@ -45,8 +45,8 @@ def test_model_with_incorrect_input_features():
 
 def test_number_of_output_features_is_correct():
     """Test the model produces the correct number of output features."""
-    model = DenseNetwork(10, 2, hidden_sizes=(5, 5, 5))
+    model = FCNet(10, 2, hidden_sizes=(5, 5, 5))
     assert model(rand(2, 10)).shape == (2, 2)
 
-    model = DenseNetwork(10, 128, hidden_sizes=(5, 5, 5))
+    model = FCNet(10, 128, hidden_sizes=(5, 5, 5))
     assert model(rand(2, 10)).shape == (2, 128)
