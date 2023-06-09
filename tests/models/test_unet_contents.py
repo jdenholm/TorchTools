addbf9166408e28956bfed52649bc7608a5b7e9b
jdenholm <j.denholm.2017@gmail.com>
2023-06-09 23:48:50 +0100

    :white_check_mark: Adding tests for UNet contents

--- tests/models/test_unet_contents.py
+++ tests/models/test_unet_contents.py
@@ -1,7 +1,24 @@
 """Test the contents of the ``UNet``."""
 from itertools import product
 
+from torch.nn import Module, LeakyReLU, Conv2d
+
 from torch_tools import UNet
+
+
+# pylint: disable=cell-var-from-loop
+
+
+def leaky_relu_slope_check(layer: Module, slope: float):
+    """Check all ``LeakyRelU``s have the correct negative slope."""
+    if isinstance(layer, LeakyReLU):
+        assert layer.negative_slope == slope
+
+
+def kernel_size_check(layer: Module, size: int):
+    """Check the kernel size in ``Conv2d`` blocks."""
+    if isinstance(layer, Conv2d):
+        assert layer.kernel_size == (size, size)
 
 
 def test_in_conv_contents_with_all_relevant_args():
@@ -48,3 +65,20 @@ def test_out_conv_contents():
 
         assert model.out_conv.in_channels == feats
         assert model.out_conv.out_channels == out_chans
+
+
+def test_leaky_relu_slopes():
+    """Test the negative slopes of the ``LeakyReLU`` layers."""
+    for slope in [0.0, 0.1, 0.2]:
+        model = UNet(in_chans=1, out_chans=1, lr_slope=slope)
+        model.apply(lambda x: leaky_relu_slope_check(x, slope))
+
+
+def test_kernel_sizes_of_relevant_components():
+    """Test the kernel sizes of the relevant components."""
+    for kernel_size in [1, 3, 5, 7]:
+        model = UNet(in_chans=1, out_chans=1, kernel_size=kernel_size)
+
+        model.in_conv.apply(lambda x: kernel_size_check(x, kernel_size))
+        model.down_blocks.apply(lambda x: kernel_size_check(x, kernel_size))
+        model.up_blocks.apply(lambda x: kernel_size_check(x, kernel_size))
