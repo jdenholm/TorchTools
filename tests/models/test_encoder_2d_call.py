7c7ac31b5766f5415c2bf8f14d430e0abb22e022
GitHub Action <action@github.com>
2023-08-26 14:33:57 +0100

    :white_check_mark: Test encoder 2d call with max_feats args

--- tests/models/test_encoder_2d_call.py
+++ tests/models/test_encoder_2d_call.py
@@ -14,6 +14,7 @@ def test_encoder_2d_return_shapes_with_brute_force_arg_combos():
     pool_style = ["avg", "max"]
     lr_slope = [0.0, 0.1]
     kernel_sizes = [1, 3, 5]
+    max_features = [None, 256, 1024]
 
     iterator = product(
         in_chans,
@@ -22,10 +23,19 @@ def test_encoder_2d_return_shapes_with_brute_force_arg_combos():
         pool_style,
         lr_slope,
         kernel_sizes,
+        max_features,
     )
 
-    for in_chans, feats, blocks, pool, slope, kernel_size in iterator:
-        model = Encoder2d(in_chans, feats, blocks, pool, slope, kernel_size)
+    for in_chans, feats, blocks, pool, slope, kernel_size, max_feats in iterator:
+        model = Encoder2d(
+            in_chans,
+            feats,
+            blocks,
+            pool,
+            slope,
+            kernel_size,
+            max_feats=max_feats,
+        )
 
         out = model(rand(1, in_chans, 32, 64))
 
