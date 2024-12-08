ea8e5b98161f87f2b63a7b72e1c42cd7ad64c4ab
jdenholm <j.denholm.2017@gmail.com>
2024-12-08 18:51:54 +0000

    :roating_light:

--- src/torch_tools/datasets/_dataset.py
+++ src/torch_tools/datasets/_dataset.py
@@ -143,8 +143,7 @@ class DataSet(_BaseDataset):
             `x_item` mapped to a tensor.
 
         """
-        # type: ignore
-        return self._x_tfms(x_item) if self._x_tfms is not None else x_item
+        return self._x_tfms(x_item) if self._x_tfms is not None else x_item  # type: ignore
 
     def _apply_target_transforms(
         self, y_item: Union[str, Path, Tensor, ndarray]
@@ -162,8 +161,7 @@ class DataSet(_BaseDataset):
             `y_item` mapped to a tensor.
 
         """
-        # type: ignore
-        return self._y_tfms(y_item) if self._y_tfms is not None else y_item
+        return self._y_tfms(y_item) if self._y_tfms is not None else y_item  # type: ignore
 
     def _apply_both_tfms(
         self,
